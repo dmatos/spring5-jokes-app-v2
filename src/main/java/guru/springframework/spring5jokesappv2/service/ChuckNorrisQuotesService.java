287eda76100c5d610a9dd9f36ed0916cd28f68a7
dmatos <dmatos88@gmail.com>
2021-08-29 18:37:35 -0300

    interface do serviço e dependencia adicionada

---
+++ src/main/java/guru/springframework/spring5jokesappv2/service/ChuckNorrisQuotesService.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring5jokesappv2.service;
+
+public class ChuckNorrisQuotesService implements QuotesService {
+    @Override
+    public String getQuote() {
+        return null;
+    }
+}
