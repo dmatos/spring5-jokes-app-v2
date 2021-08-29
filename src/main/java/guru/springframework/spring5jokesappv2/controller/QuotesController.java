2db1319cb3dff13c6dce3e80a2e3574f9a385ab8
dmatos <dmatos88@gmail.com>
2021-08-29 19:44:02 -0300

    /quote/chuck-norris

---
+++ src/main/java/guru/springframework/spring5jokesappv2/controller/QuotesController.java
@@ -0,0 +1,23 @@
+package guru.springframework.spring5jokesappv2.controller;
+
+import guru.springframework.spring5jokesappv2.service.ChuckNorrisQuotesService;
+import guru.springframework.spring5jokesappv2.service.QuotesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class QuotesController {
+
+    final private QuotesService service;
+
+    public QuotesController(QuotesService service){
+        this.service = service;
+    }
+
+    @RequestMapping("/quote/chuck-norris")
+    public String getQuote(Model model){
+        model.addAttribute("quote", this.service.getQuote());
+        return "quote";
+    }
+}
--- src/main/java/guru/springframework/spring5jokesappv2/service/ChuckNorrisQuotesService.java
+++ src/main/java/guru/springframework/spring5jokesappv2/service/ChuckNorrisQuotesService.java
@@ -1,8 +1,12 @@
 package guru.springframework.spring5jokesappv2.service;
 
-public class ChuckNorrisQuotesService implements QuotesService {
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChuckNorrisQuotesService extends ChuckNorrisQuotes implements QuotesService{
     @Override
     public String getQuote() {
-        return null;
+        return getRandomQuote();
     }
 }
